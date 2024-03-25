e58e809e2a042f418181d680177a8b6ad02f1a69
Zahlkovsk1 <148435929+Zahlkovsk1@users.noreply.github.com>
2024-03-25 17:17:11 +0100

    error with fetchArticles fixed

--- rozella/FeedReed/fetchArticles.swift
+++ rozella/FeedReed/fetchArticles.swift
@@ -1,8 +1,22 @@
-//
-//  fetchArticles.swift
-//  rozella
-//
-//  Created by SHOHJAHON on 25/03/24.
-//
-
+import FeedKit
 import Foundation
+
+func fetchArticles(from feed: RSSFeed, completion: @escaping ([Article]) -> Void) {
+    let parser = FeedParser(URL: feed.url)
+    parser.parseAsync { result in
+        switch result {
+        case .success(let feed):
+            let articles = feed.rssFeed?.items?.map { item in
+                Article(title: item.title ?? "",
+                        description: item.description ?? "",
+                        link: URL(string: item.link ?? "https://example.com") ?? URL(string: "https://example.com")!,
+                        scrapedContent: nil) // Initializing with nil
+            } ?? []
+            completion(articles)
+        case .failure(let error):
+            print(error)
+            completion([])
+        }
+    }
+}
+
