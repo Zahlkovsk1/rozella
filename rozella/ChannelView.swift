77d4fdd487e0d06126adf229594a4f3ad4d0c415
Zahlkovsk1 <148435929+Zahlkovsk1@users.noreply.github.com>
2024-03-25 15:32:48 +0100

    channel information, and url done

---
+++ rozella/ChannelView.swift
@@ -0,0 +1,18 @@
+//
+//  ChannelView.swift
+//  rozella
+//
+//  Created by SHOHJAHON on 25/03/24.
+//
+
+import SwiftUI
+
+struct ChannelView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ChannelView()
+}
--- rozella/YoutubeSearchView.swift
+++ rozella/YoutubeSearchView.swift
@@ -5,17 +5,54 @@ struct YoutubeSearchView: View {
     private var YTM = YouTubeModel()
     @State private var text: String = ""
     @State private var searchResults: [SearchResponse] = []
-
+    @State private var searchResponse: SearchResponse?
+    
     var body: some View {
         
-            VStack {
+        VStack {
+            
+            if let validSearchResults = searchResponse?.results{
+                ForEach(validSearchResults.indices, id: \.self){ index in
+                    if let channel = validSearchResults[index] as? YTChannel{
+                        HStack{
+                            Text(channel.name ?? "N/A")
+                            
+                            Text(channel.subscriberCount ?? "N/A")
+                            
+                            Text("https://www.youtube.com/channel/\(channel.channelId)")
+                            
+                        }
+                        //                            .onAppear(){
+                        //                                print("https://www.youtube.com/channel/\(channel.channelId)")
+                        //                            }
+                        .onAppear(){
+                            dump(channel.channelId)
+                            
+                            let channelId: String = channel.channelId
+                            
+                            Task {
+                                do {
+                                    let searchResponse = try await ChannelInfosResponse.sendRequest(youtubeModel: YTM, data: [.browseId: channelId])
+                                    
+                                    dump(searchResponse)
+                                    
+                                } catch {
+                                    print(error)
+                                }
+                            }
+                        }
+                    }
+                    
+                }
+                
+                
                 TextField("Search", text: $text)
-
+                
                 Button {
                     Task {
                         do {
-                            let searchResponse = try await SearchResponse.sendRequest(youtubeModel: YTM, data: [.query: text])
-                            print(searchResponse)
+                            searchResponse = try await SearchResponse.sendRequest(youtubeModel: YTM, data: [.query: text])
+                            
                         } catch {
                             print(error)
                         }
@@ -23,11 +60,11 @@ struct YoutubeSearchView: View {
                 } label: {
                     Text("Search")
                 }
-                 
+                
             }
         }
     }
-
+}
 
 #Preview {
     YoutubeSearchView()
