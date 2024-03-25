536145741927f38bf04fd49299c3192f64303aa9
Zahlkovsk1 <148435929+Zahlkovsk1@users.noreply.github.com>
2024-03-25 15:50:10 +0100

    slightly improved youtubeSearch

--- rozella/YoutubeSearchView.swift
+++ rozella/YoutubeSearchView.swift
@@ -6,48 +6,48 @@ struct YoutubeSearchView: View {
     @State private var text: String = ""
     @State private var searchResults: [SearchResponse] = []
     @State private var searchResponse: SearchResponse?
-    
+
     var body: some View {
         
-        VStack {
-            
-            if let validSearchResults = searchResponse?.results{
-                ForEach(validSearchResults.indices, id: \.self){ index in
-                    if let channel = validSearchResults[index] as? YTChannel{
-                        HStack{
-                            Text(channel.name ?? "N/A")
-                            
-                            Text(channel.subscriberCount ?? "N/A")
-                            
-                            Text("https://www.youtube.com/channel/\(channel.channelId)")
-                            
-                        }
-                        //                            .onAppear(){
-                        //                                print("https://www.youtube.com/channel/\(channel.channelId)")
-                        //                            }
-                        .onAppear(){
-                            dump(channel.channelId)
-                            
-                            let channelId: String = channel.channelId
-                            
-                            Task {
-                                do {
-                                    let searchResponse = try await ChannelInfosResponse.sendRequest(youtubeModel: YTM, data: [.browseId: channelId])
-                                    
-                                    dump(searchResponse)
-                                    
-                                } catch {
-                                    print(error)
+            VStack {
+                
+                if let validSearchResults = searchResponse?.results{
+                    ForEach(validSearchResults.indices, id: \.self){ index in
+                        if let channel = validSearchResults[index] as? YTChannel{
+                            HStack{
+                                Text(channel.name ?? "N/A")
+                                
+                                Text(channel.subscriberCount ?? "N/A")
+                                
+                                Text( "https://www.youtube.com/channel/\(channel.channelId)")
+                            }
+                            .onAppear(){
+                                print("https://www.youtube.com/channel/\(channel.channelId)")
+
+                            }
+                            .onAppear(){
+                                dump(channel.channelId)
+                                
+                                let channelId: String = channel.channelId
+                                
+                                Task {
+                                    do {
+                                        let searchResponse = try await ChannelInfosResponse.sendRequest(youtubeModel: YTM, data: [.browseId: channelId])
+                                        
+                                        dump(searchResponse)
+                                        
+                                    } catch {
+                                        print(error)
+                                    }
                                 }
                             }
                         }
                     }
-                    
                 }
-                
+
                 
                 TextField("Search", text: $text)
-                
+
                 Button {
                     Task {
                         do {
@@ -60,12 +60,9 @@ struct YoutubeSearchView: View {
                 } label: {
                     Text("Search")
                 }
-                
+                 
             }
         }
     }
-}
 
-#Preview {
-    YoutubeSearchView()
-}
+
