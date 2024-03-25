345113d59980cd808f0f13b88f614da2f3670310
Zahlkovsk1 <148435929+Zahlkovsk1@users.noreply.github.com>
2024-03-25 13:06:02 +0100

    Initial Commit

---
+++ rozella/rozellaApp.swift
@@ -0,0 +1,17 @@
+//
+//  rozellaApp.swift
+//  rozella
+//
+//  Created by SHOHJAHON on 25/03/24.
+//
+
+import SwiftUI
+
+@main
+struct rozellaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
