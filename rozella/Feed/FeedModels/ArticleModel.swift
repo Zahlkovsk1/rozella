8dd350bbe3677ef1dcbbedf719147ef2939cd8f0
Zahlkovsk1 <148435929+Zahlkovsk1@users.noreply.github.com>
2024-05-14 10:23:50 +0200

    article model restractured

--- rozella/Feed/FeedModels/ArticleModel.swift
+++ rozella/Feed/FeedModels/ArticleModel.swift
@@ -1,12 +1,37 @@
 
 import SwiftUI
 import FeedKit
+import SwiftSoup
+import OSLog
 
 struct Article: Identifiable {
+    
     let id = UUID()
-    let title: String
-    let description: String
-    var link: URL
-    var scrapedContent: String?
-    var image: RSSFeedImage
+    let link: URL
+    var title: String
+    var description: String
+    var document: Document? = nil
+    var image: RSSFeedImage? = nil
+    
+    init(
+        link: URL,
+        title: String? = nil,
+        description: String? = nil,
+        contentEncoded: String? = nil,
+        image: RSSFeedImage? = nil
+    ) {
+        self.link = link
+        self.title = title ?? "Untitled"
+        self.description = description ?? ""
+        self.image = image
+        
+        if let contentEncoded = contentEncoded {
+            do {
+                self.document = try Parser.parse(contentEncoded, link.absoluteString)
+            } catch {
+                Logger.parser.error("\(String(describing: error))")
+            }
+        }
+    }
+    
 }
