a2708ac0cf4f88ee74ba2f0da02138e844f32e99
Zahlkovsk1 <148435929+Zahlkovsk1@users.noreply.github.com>
2024-05-14 16:23:59 +0200

    two SwiftSoupFunctions added

--- rozella/Feed/FeedModels/SwiftSoupFunctions.swift
+++ rozella/Feed/FeedModels/SwiftSoupFunctions.swift
@@ -5,4 +5,54 @@
 //  Created by SHOHJAHON on 14/05/24.
 //
 
-import Foundation
+import SwiftUI
+import SwiftSoup
+import OSLog
+
+    @ViewBuilder
+    func renderDocument(_ document: Document?) -> some View {
+        if let body = document?.body() {
+        renderTag(body)
+        } else {
+            EmptyView()
+        }
+    }
+
+
+
+    
+    func renderTag(_ element: Elements.Element) -> some View {
+    
+        var elementText: String?
+    
+        let childrenCount: Int = element.children().count
+    
+        if childrenCount == 0 {
+        
+            do {
+                elementText = try element.text()
+            } catch {
+            Logger.view.error("Could not parse this element text.")
+            }
+        
+            return if let text = elementText {
+                AnyView (
+                    Text(text)
+                        .style(element.tagName())
+                )
+            } else {
+                AnyView(
+                    EmptyView()
+                )
+            }
+        
+        } else {
+        
+            return AnyView (
+                ForEach(element.children(), id: \.self) { child in
+                    renderTag(child)
+                }
+            )
+        
+        }
+    }
